f797075b4585585a33c8fba10b64ebbde1be44ad
Benny <17568838+Diggen85@users.noreply.github.com>
2026-05-18 11:46:48 +0200

    Remove copy_certs function from certbot-renew.sh move to deploy hook
    Removed the copy_certs function and its invocation.

--- certbot-renew.sh
+++ certbot-renew.sh
@@ -7,15 +7,6 @@ set -e
 CERT_DIR="/etc/letsencrypt/live/${DOMAIN}"
 MOSQUITTO_CERT_DIR="/mosquitto/certs"
 
-copy_certs() {
-  cp "${CERT_DIR}/fullchain.pem" "${MOSQUITTO_CERT_DIR}/fullchain.pem"
-  cp "${CERT_DIR}/privkey.pem" "${MOSQUITTO_CERT_DIR}/privkey.pem"
-
-  chmod 640 "${MOSQUITTO_CERT_DIR}"/*.pem
-
-  pkill -HUP mosquitto || true
-}
-
 # create initial Certificate if missing
 if [ ! -f "${CERT_DIR}/fullchain.pem" ]; then
   certbot certonly \
@@ -34,6 +25,3 @@ certbot renew \
 
 # Display Cert
 certbot certonly --cert-name "${DOMAIN}"
-
-# Copy Certs to Mosquitto
-copy_certs
