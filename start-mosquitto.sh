89fb7340bbda208eb261e19e5499500771d0e473
Benny <17568838+Diggen85@users.noreply.github.com>
2026-05-20 12:09:06 +0200

    Update file paths for password and certificate checks

--- start-mosquitto.sh
+++ start-mosquitto.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-while [ ! -f /mosquitto/config/passwordfile ]; do
+while [ ! -f /etc/mosquitto/passwordfile ]; do
   echo "Waiting for passwordfile..."
   sleep 2
 done
 
-while [ ! -f /mosquitto/certs/server.pem ]; do
+while [ ! -f /etc/mosquitto/certs/server.pem ]; do
   echo "Waiting for certificates..."
   sleep 2
 done
 
-while [ ! -f /mosquitto/certs/server.key ]; do
+while [ ! -f/etc/mosquitto/certs/server.key ]; do
   echo "Waiting for certificate keys..."
   sleep 2
 done
